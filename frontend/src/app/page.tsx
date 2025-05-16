3f22f3bb18f38a164c1bad636fb32e9f0cef1779
Projacktor <1234xl.com@gmail.com>
2025-05-16 16:00:07 +0300

    chore: install new dependencies

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,1 +1,19 @@
-export default function Home() {}
+
+export default function Home() {
+    return (
+        <main className="w-screen h-screen flex items-center justify-center flex-col">
+            <section className="flex flex-row items-center justify-around w-[80%] gap-8">
+                <div className="flex flex-col items-center space-y-2">
+                    <h3 className="text-3xl text-background">
+                        Files to download
+                    </h3>
+                </div>
+                <div className="flex flex-col items-center space-y-2">
+                    <h3 className="text-3xl text-background">
+                        Your uploaded files
+                    </h3>
+                </div>
+            </section>
+        </main>
+    )
+}
