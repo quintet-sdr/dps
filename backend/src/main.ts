bdc22f4f0044f2ed9acde6c1751fd832fa9ce42a
Projacktor <1234xl.com@gmail.com>
2025-05-18 00:51:22 +0300

    feat: add swagger documentation
    Took 9 minutes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,20 @@ import * as crypto from 'crypto';
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Endpoints description')
+    .setDescription('The cast API description')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(process.env.PORT ?? 8000);
