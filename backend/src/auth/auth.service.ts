5c156d3a7a73c6b1cf3e3e6cc0a5916fc34d9c9c
Projacktor <1234xl.com@gmail.com>
2025-05-17 14:17:49 +0300

    feat: add docker-compose with database and user resource
    Took 37 minutes

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  async validateUser(email: string, pass: string) {}
+}
